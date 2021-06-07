2fef5cd623f4cc07800b0bf911f02380d480b9fb
Guruprasath4401 <guruprasathindirakumar@gmail.com>
2021-06-07 13:09:22 +0530

    Updated Main file

--- Fcms/src/Dbconnector.java
+++ Fcms/src/Dbconnector.java
@@ -9,6 +9,7 @@ public class Dbconnector {
 	 ResultSet myRs=null;	
 	 Connection myConn = null;
 	 Statement myStmt = null;
+	 
 	public Dbconnector() 
 	{
 	
@@ -31,44 +32,36 @@ public class Dbconnector {
 		
 			while (myRs.next()) {
 				System.out.println(myRs.getString("username") + " , " + myRs.getString("title") + " , " + myRs.getString("ratings"));
-			}	
+				
+			}
+			System.out.println("");
 			}
 			catch (Exception exc) {
 				exc.printStackTrace();
 			}
 			
 		}
-		public void insert( String username,String userph,String title,String year,String lang,String dir,int ratings) {
-			try {
-				//System.out.println("Inserting a new record to database\n");
-				String user_name=username;
-				String user_ph=userph;
-				String tit=title;
-				String yr=year;
-				String lan=lang;
-				String dirt=dir;
-				int rating=ratings;
-				
-				
+		public void insert( String username,String userph,String title,String year,String language,String director,int ratings) {
+			
+			try {	
 			int rowsAffected = myStmt.executeUpdate(
 				"insert into fcms " +
 			"(username,userph,title,year,language,Director,ratings) " + 
 			"values " + 
-			 "( '"+user_name+"', '"+user_ph+"','"+tit+"','"+yr+"','"+lan+"', '"+dirt+"','"+rating+"')");
+			 "( '"+username+"', '"+userph+"','"+title+"','"+year+"','"+language+"', '"+director+"','"+ratings+"')");
 			 
 			}
 			catch (Exception exc) {
 				exc.printStackTrace();
 			}
 			
+			
 		}
 		public void countRec(String title,String year) 
 		{
 			try {
-				String tit=title;
-				String yr=year;
 			 String query=null;
-			 query= "SELECT COUNT(*) FROM fcms where title= '"+tit+"'  AND year = '"+yr+"' ";
+			 query= "SELECT COUNT(*) FROM fcms where title= '"+title+"'  AND year = '"+year+"' ";
 			 myRs =myStmt.executeQuery(query);
 			 myRs.next();
 			 int count = myRs.getInt(1);
@@ -81,14 +74,8 @@ public class Dbconnector {
 		}
 		public void average(String title,String year) {
 			try {
-				String tit=title;
-				String yr=year;
-            // myRs = myStmt.executeQuery("select * from user");
-			 
-			 
-			 myRs =myStmt.executeQuery("select ROUND(avg(ratings),1) from fcms  where title= '"+tit+"'  AND year = '"+yr+"'");
+			 myRs =myStmt.executeQuery("select ROUND(avg(ratings),1) from fcms  where title= '"+title+"'  AND year = '"+year+"'");
 			 myRs.next();
-			 //float co = myRs.getInt(1);
 			 System.out.println("avg :"+ myRs.getDouble(1));
 		    }
 			catch (Exception exc) {
@@ -96,20 +83,18 @@ public class Dbconnector {
 			}
 	}
 	 public static void main(String[] args)  {
-
-		//Testdb  db =new Testdb();
 		
 	}  	
 }
 //MYSQL TABLE
 //CREATE TABLE `fcms`.`fcms` (
 //		  `username` VARCHAR(20) NOT NULL,
-//		  `userph` VARCHAR(13) NULL,
-//		  `title` VARCHAR(45) NULL,
-//		  `year` VARCHAR(4) NULL,
-//		  `language` VARCHAR(45) NULL,
-//		  `Director` VARCHAR(45) NULL,
-//		  `ratings` INT NULL,
+//		  `userph` VARCHAR(13) NOT NULL,
+//		  `title` VARCHAR(45)NOT NULL,
+//		  `year` VARCHAR(4) NOT NULL,
+//		  `language` VARCHAR(45) NOT NULL,
+//		  `Director` VARCHAR(45) NOT  NULL,
+//		  `ratings` INT NOT NULL,
 //		  PRIMARY KEY (`username`));
 //
 //		
--- Fcms/src/Main.java
+++ Fcms/src/Main.java
@@ -2,53 +2,54 @@ import java.util.*;
 import java.util.Scanner;
 import java.io.*;
 class Login{
-    String username, userph, title,year, lang, dir;
+    String username, userph, title,year, language, director;
     int ratings;
-   Dbconnector t =new  Dbconnector();
-    Login(String username,String userph,String title,String year,String lang,String dir,int ratings){
+    Dbconnector data =new Dbconnector();
+    Login(String username,String userph,String title,String year,String language,String director,int ratings){
         this.username=username;
         this.userph=userph;
         this.title=title;
         this.year=year;
-        this.lang=lang;
-        this.dir=dir;
+        this.language=language;
+        this.director=director;
         this.ratings=ratings;
     
     }
 	Scanner in=new Scanner(System.in);
-	void Admin(){
+	public void admin(){
 		System.out.println("**********ADMIN**********");  
 		System.out.print("\nAdmin name : ");
-		String admin=in.nextLine();
+		String admin_name=in.nextLine();
+		
 		System.out.print("Password  : ");
-		String adpass=in.nextLine();
-		String ad="admin";
-		String pass="admin";
-		if(admin.equals(ad) && adpass.equals(pass))
+		String admin_password=in.nextLine();
+		String name="admin";
+		String password="admin123";
+		if(admin_name.equals(name) && admin_password.equals(password))
 		{
 		System.out.println("\nWelcome Admin");
 		System.out.println("\nFeedback Details");
-		t.getData();
+		data.getData();
 		}
 		else{
 		System.out.println("\nInvalid name or password");
 		}
 	}
-	void user(){
+	public void user(){
 		System.out.println("Welcome To Feedback Collection Management System");  
-		System.out.print("\nUser name : ");
+		System.out.print("\nEnter your user name : ");
 		String username=in.nextLine();
-		System.out.print("Phone number  : ");
+		System.out.print("Enter your phone number  : ");
 		String userph=in.nextLine();
-		System.out.print("Enter the Movie name: ");
+		System.out.print("Enter the movie name: ");
 		String title=in.nextLine();
 		System.out.print("Released year: ");
 		String year=in.nextLine();
 		System.out.print("Language : ");
-		String lang=in.nextLine();
+		String language=in.nextLine();
 		System.out.print("Director name : ");
-		String dir=in.nextLine();
-		
+		String director=in.nextLine();
+	if(username !="" && userph != "" && title != "" && year !="" && language !="" && director != "") {
 		System.out.println("\n******Star Rating******");
     	 System.out.println(" 5.***** ");
     	 System.out.println(" 4.**** ");
@@ -56,17 +57,21 @@ class Login{
     	 System.out.println(" 2.** ");
     	 System.out.println(" 1.* ");
     	 System.out.println("\nHello "+ username);
-    	 System.out.println("\nEnter the number to select your Ratings for "+ title);
+    	 System.out.println("\nEnter a number between 1 and 5 to select your ratings for "+ title);
     	 int rating=in.nextInt();
     	 in.nextLine();
     	 calculate(rating,title);
-    	 t.insert(username, userph,title,year,lang, dir,rating);
-    	 t.countRec(title,year);
-    	 t.average(title, year);
-    	
-        System.out.println("\nThank you For your feedback !!!");
+    	 data.insert(username, userph,title,year,language, director,rating);
+    	 data.countRec(title,year);
+    	 data.average(title, year);
+    	 System.out.println("\nThank you For your feedback !!!");
+	}else {
+		System.out.println("\nPlease try again...\n");
+		user();
 	}
-	void calculate (int rating,String title)
+        
+	}
+	public void calculate (int rating,String title)
     {
         if(rating==1 )
         {
@@ -99,24 +104,32 @@ class Login{
             String userph="";
             String title="";
             String year="";
-            String lang="";
-            String dir="";
+            String language="";
+            String director="";
             int ratings=0;
-        Login m=new Login(username,userph,title, year,lang,dir, ratings);
+        Login login=new Login(username,userph,title, year,language,director, ratings);
         Scanner in=new Scanner(System.in);
-        System.out.println("1.Admin\n2.User\nEnter your choice: ");
-        int c=in.nextInt();
+        int c=0;
+        do {
+        System.out.println("1.Admin\n2.User\n3.Exit\nEnter your choice: ");
+         c=in.nextInt();
         switch(c){
         	case 1:
-        	m.Admin();
-        	break;
+        		login.admin();
+        		break;
         	case 2:
-        	m.user();
-        	break;
+        		login.user();
+        		break;
+        	case 3:
+        		System.out.println("\nExcited");
+            	System.exit(0);
+            	
+            	break;
         	default:
-        	System.out.println("Invalid Input");
+        		System.out.println("\nInvalid Input\n");
         }
-        in.close();
+        }while(c!=0);
+        
        
         
 
